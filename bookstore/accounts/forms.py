8dfcf09ea27ae45613eea183ee35ddf79795d249
DParra657 <Diegoparra657@gmail.com>
2025-07-05 15:59:56 -0400

    PRofile,sign,login,home

--- bookstore/accounts/forms.py
+++ bookstore/accounts/forms.py
@@ -3,22 +3,39 @@ from .models import Users
 from django.contrib.auth.forms import PasswordChangeForm
 
 class SignUpForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-auth'}))
 
     class Meta:
         model = Users
         fields = ['email', 'name', 'phone', 'shipping_address', 'card_type', 'card_number', 'expiration_date', 'password']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'input-auth'}),
+            'name': forms.TextInput(attrs={'class': 'input-auth'}),
+            'phone': forms.TextInput(attrs={'class': 'input-auth'}),
+            'shipping_address': forms.Textarea(attrs={'class': 'input-auth', 'rows': 3}),
+            'card_type': forms.TextInput(attrs={'class': 'input-auth'}),
+            'card_number': forms.TextInput(attrs={'class': 'input-auth'}),
+            'expiration_date': forms.TextInput(attrs={'class': 'input-auth'}),
+        }
 
 class LoginForm(forms.Form):
-    identifier = forms.CharField(label="Email or Account ID")
-    password = forms.CharField(widget=forms.PasswordInput)
+    identifier = forms.CharField(label="Email or Account ID", widget=forms.TextInput(attrs={'class': 'input-auth'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-auth'}))
 
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Users
         fields = ['name', 'phone', 'shipping_address', 'card_type', 'card_number', 'expiration_date']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'input-auth'}),
+            'phone': forms.TextInput(attrs={'class': 'input-auth'}),
+            'shipping_address': forms.Textarea(attrs={'class': 'input-auth', 'rows': 3}),
+            'card_type': forms.TextInput(attrs={'class': 'input-auth'}),
+            'card_number': forms.TextInput(attrs={'class': 'input-auth'}),
+            'expiration_date': forms.TextInput(attrs={'class': 'input-auth'}),
+        }
 
 class CustomPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(widget=forms.PasswordInput)
-    new_password1 = forms.CharField(widget=forms.PasswordInput)
-    new_password2 = forms.CharField(widget=forms.PasswordInput)
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-auth'}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-auth'}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-auth'}))
