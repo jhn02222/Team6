159f73c08a4369cf80edcd10b26a536cb7bf61c8
chaston <ckc92424@uga.edu>
2025-07-04 20:12:05 -0400

    add book page layout

--- bookstore/store/forms.py
+++ bookstore/store/forms.py
@@ -5,3 +5,20 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+        widgets = {
+            'isbn': forms.TextInput(attrs={'class': 'form-input'}),
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'author': forms.TextInput(attrs={'class': 'form-input'}),
+            'category': forms.TextInput(attrs={'class': 'form-input'}),
+            'edition': forms.TextInput(attrs={'class': 'form-input'}),
+            'publisher': forms.TextInput(attrs={'class': 'form-input'}),
+            'publication_year': forms.NumberInput(attrs={'class': 'form-input'}),
+            'cover_image': forms.URLInput(attrs={'class': 'form-input'}),
+            'quantity_in_stock': forms.NumberInput(attrs={'class': 'form-input'}),
+            'minimum_threshold': forms.NumberInput(attrs={'class': 'form-input'}),
+            'buying_price': forms.NumberInput(attrs={'class': 'form-input'}),
+            'selling_price': forms.NumberInput(attrs={'class': 'form-input'}),
+            'is_featured': forms.CheckboxInput(attrs={'class': 'form-checkbox'}),
+            'release_date': forms.DateInput(attrs={'class': 'form-input', 'type': 'date'}),
+        }
+       
